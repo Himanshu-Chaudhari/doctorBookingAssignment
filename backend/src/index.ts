a35955cbefc54130aae56e0786037c7b2e7c584a
Himanshu Suresh Chaudhari <himanshuchaudhari@Himanshus-MacBook-Air.local>
2024-12-28 01:59:35 +0530

    Trying to fix cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin : process.env.FRONTEND_URL
+}));
+
 app.use(bodyParser.json());
 app.use(express.json());
 
